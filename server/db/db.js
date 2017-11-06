f45aa53399231261f0822c88d5b31a9c7bf87222
Jeanne Castillo <jcaron27@gmail.com>
2017-11-05 19:41:48 -0500

    add dialect to sequelize setup

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
   process.env.DATABASE_URL || 'postgres://localhost:5432/pickled_dreams', {
-    logging: false
+    logging: false,
+    dialect: 'postgres',
+    ssl: true
   }
 )
 module.exports = db
