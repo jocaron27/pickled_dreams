6a0c0ca83d19fce109bb4de339df8017b7349120
Jeanne Castillo <jcaron27@gmail.com>
2017-11-05 19:56:34 -0500

    edit db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,9 @@
 const Sequelize = require('sequelize')
-const pkg = require('../package.json');
+const pkg = require('../../package.json');
 const name = process.env.DATABASE_NAME || pkg.name;
 const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${name}`;
 const db = new Sequelize(connectionString, {
-  logging: false, // export DEBUG=sql in the environment to get SQL queries
-  native: true    // lets Sequelize know we can use pg-native for ~30% more speed (if you have issues with pg-native feel free to take this out and work it back in later when we have time to help)
+  logging: false    // lets Sequelize know we can use pg-native for ~30% more speed (if you have issues with pg-native feel free to take this out and work it back in later when we have time to help)
 });
 
 module.exports = db;
